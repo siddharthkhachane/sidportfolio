7026f287b01c0fa5a3c0206854069fdcad7d77bb
Siddharth Samir Khachane <khachane@usc.edu>
2025-04-20 01:01:22 -0700

    Update Hero.js

--- src/components/sections/Hero.js
+++ src/components/sections/Hero.js
@@ -79,7 +79,7 @@ const Hero = () => {
             animate={{ opacity: 1, y: 0 }}
             transition={{ duration: 0.5, delay: 0.4 }}
           >
-            I'm a <RoleText ref={textRef}></RoleText><Cursor color={currentColor}>|</Cursor>
+            I'm a  <RoleText ref={textRef}></RoleText><Cursor color={currentColor}>|</Cursor>
           </RoleWrapper>
           
           <Description
